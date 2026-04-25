ecbd247b63d25f2a6681e85210f5b558e8793c58
vuquanghuy1998 <vuquanghuy1998@gmail.com>
2026-04-25 20:27:08 +1000

    initial commit

---
+++ BubblePop/BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by Quang Huy Vu on 25/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
